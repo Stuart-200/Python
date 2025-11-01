492f4f14731e85c5f3ddb6a7ef7becb2337f9042
Stuart-200 <swilson.2009@zohomail.com>
2025-11-01 22:02:15 +0600

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,17 @@
+#Keyword, Variable and Indentation.
+name="John"
+age= "30" 
+is_student= True
+weight = 70.5
+
+print("Data type of Name is:",type(name))
+print("Data type of Age is:",type(age))
+print("The age is:", age)
+
+print('before typecasting')
+print("Data Type of age is:",type(age))
+print()
+
+print('after typecasting')
+newage=int(age)
+print("Data Type of newage is:",type(newage))
