d8fd41a6e68fa83cda36c85ed58b220e1d0af3b6
Stuart-200 <swilson.2009@zohomail.com>
2025-11-01 22:27:11 +0600

    Add files via upload

---
+++ string.py
@@ -0,0 +1,14 @@
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + last_name
+
+
+print("First Name:", first_name)
+print("Full Name:", full_name)
+
+
+word = "Codingal"
+
+print("length of string :", len(word))
+print("Sliced String:", word[0:3])
+print("Last Letter: ", word[5])
