483739e0f153c26dc1fa1e6df406cbc077c57e1d
Stuart-200 <swilson.2009@zohomail.com>
2025-11-01 22:17:46 +0600

    Add files via upload

---
+++ operater.py
@@ -0,0 +1,11 @@
+num1=45
+num2= 3
+
+
+print("Floor Division:", num1//num2)
+print("Modulus Operation:", num1%num2)
+print("Square:", num2**2)
+
+print("Equal ?", num1==num2)
+print("Number 1 greater?", num1>num2)
+print("Not Equal ?", num1!=num2)
