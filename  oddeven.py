a336fd8b304a5d120be122b1704d440380387032
Stuart-200 <swilson.2009@zohomail.com>
2025-11-07 21:54:22 +0600

    Add files via upload

---
+++  oddeven.py
@@ -0,0 +1,12 @@
+number =int(input("Enter a number"))
+print("Number to be checked :" , number)
+
+
+
+
+
+if number%2 == 0:
+    print("the number is even")
+
+else:
+    print("the number is odd")
