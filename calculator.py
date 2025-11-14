4568008009ee387ad728ac1b2c851c045dad58da
Stuart-200 <swilson.2009@zohomail.com>
2025-11-14 22:22:52 +0600

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,21 @@
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+
+def add(num1,num2):
+    return num1 + num2
+
+def subtract(num1,num2):
+    return num1 - num2
+
+def multiply(num1,num2):
+    return num1 * num2
+
+def divide(num1,num2):
+     return num1 / num2 
+ 
+ 
+ 
+print("Sum:" , add(num1,num2))
+print("Substract:" , subtract(num1,num2))
+print("Multiply:" , multiply(num1,num2))
+print("Division:" , divide(num1,num2))
