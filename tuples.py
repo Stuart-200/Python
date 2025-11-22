1c7b219e9408310cef572f13aa679fc0ee6e6320
Stuart-200 <swilson.2009@zohomail.com>
2025-11-22 22:07:13 +0600

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,22 @@
+my_tuple = ()
+print(my_tuple)
+print()
+
+my_tuple =("mouse", [8,4,6], (1,2,3))
+print(my_tuple)
+print()
+
+my_tuple = ('p', 'e', 'r', 'm', 'i', 't')
+print(my_tuple[0])
+print(my_tuple[5])
+print()
+
+n_tuple =("mouse",[8,4,6],(1, 2,3))
+print(n_tuple[2][2])
+
+
+anewtuple = ('p', 'e', 'r', 'm', 'i', 't')
+print("Sliced : ", anewtuple[1:4])
+
+for letter in (anewtuple):
+    print("Hello", letter)
