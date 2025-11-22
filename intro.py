17832a1f3fbf58c3f3f71e77c9b78a495e9c1532
Stuart-200 <swilson.2009@zohomail.com>
2025-11-22 21:01:50 +0600

    Add files via upload

--- intro.py
+++ intro.py
@@ -1,5 +1,25 @@
-print("Hello world!!")
-#this is my first command in pytohn
-print("My name is Stuart")
-#Printing a fun fact about myself
-print("I love to read books")
+def test(lst):
+    result = {}
+    for item in lst:
+        result[item[0]] = item[1:]
+    return result
+
+
+students = [
+    [1, 'Jean castro', 'v'],
+    [2, 'Lula Foster', 'VI'],
+    [3, 'Brian Howell', 'VI'],
+    [5, 'Zachary Simon', 'VII']
+]
+
+
+print("\nOriginal list of lists:")
+print(students)
+print("\nConverted to dictionary:")
+print(test(students))
+
+
+
+
+
+
