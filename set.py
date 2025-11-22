502528805973c0cd2b3b6876f1ad4a36f32842c2
Stuart-200 <swilson.2009@zohomail.com>
2025-11-22 22:26:00 +0600

    Add files via upload

---
+++ set.py
@@ -0,0 +1,25 @@
+my_set ={1,2,2,3,4,4,4,}
+print("Set :", my_set)
+
+my_set.add(6)
+print('Updated Set :', my_set)
+print()
+
+set1 = {1,2,3,4,5}
+set2 = {2,4,4,6}
+
+print("\nSet 1:",set1)
+print("Set 2:",set2)
+print()
+
+
+
+
+print("Difference :")
+print(set1.difference(set2))
+
+
+
+print()
+print("Symmetric Differnece")
+print(set1.symmetric_difference(set2))
