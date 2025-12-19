b2484ae5d72c2ba26cff5cba22206f26dc0b455d
Stuart-200 <swilson.2009@zohomail.com>
2025-12-19 21:53:24 +0600

    Add files via upload

---
+++ operation2.py
@@ -0,0 +1,9 @@
+with open('Codingal.txt', 'r') as file:
+    data = file.readlines()
+    print("Words in this file are...")
+    
+    for line in data:
+       word = line.split()
+       print(word)
+
+file.close()
---
+++ operationpart1.py
@@ -0,0 +1,3 @@
+with open('Codingal.txt' , 'w') as file:
+    file.write("Hi!, I am Stuart. I love coding.")
+file.close()
