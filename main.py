817ae378bda344313c2c297af84ce8a04c2d728b
Stuart-200 <swilson.2009@zohomail.com>
2026-01-16 19:53:09 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,55 +1,14 @@
-import tkinter as tk
-import random
+def decimal_to_binary(n):
+    if n==0:
+        return "0"
+    
+    binary = ""
+    while n > 0:
+        reminder = n%2
+        binary = str(reminder) + binary
+        n = n//2
+    return binary
 
-window = tk.Tk()
-window.title("Rock paper scissors")
-window.geometry("400x300")
-
-title = tk.Label(window, text="Rock Paper Scissors", font=("Arial", 20))
-title.pack(pady=10)
-
-result_label = tk.Label(window, text="", font=("Arial", 14))
-result_label.pack(pady=15)
-
-computer = tk.Label(window, text="", font=("Arial", 14))
-computer.pack(pady=15)
-
-choices = ["Rock", "Paper", "Scissors"]
-
-
-def play(user_choice):
-    computer_choice = random.choice(choices)
-
-    computer.config(text=f"Computer chose: {computer_choice}")
-
-    if user_choice == computer_choice:
-        result_label.config(text="It's a draw!")
-    elif (
-        (user_choice == "Rock" and computer_choice == "Scissors")
-        or (user_choice == "Paper" and computer_choice == "Rock")
-        or (user_choice == "Scissors" and computer_choice == "Paper")
-    ):
-          result_label.config(text="You win!")
-
-    else:
-        result = "You lose!"
-
-        result_label.config(text=result)
-
-
-rock_btn = tk.Button(
-    window, text="Rock🪨", font=("Arial", 14), command=lambda: play("Rock")
-)
-rock_btn.pack(pady=5)
-
-paper_btn = tk.Button(
-    window, text="Paper📄", font=("Arial", 14), command=lambda: play("Paper")
-)
-paper_btn.pack(pady=5)
-
-scissors_btn = tk.Button(
-    window, text="Scissors✂️", font=("Arial", 14), command=lambda: play("Scissors")
-)
-scissors_btn.pack(pady=5)
-
-window.mainloop()
+number = int(input("Enter a decimal number: "))
+binary_representation = decimal_to_binary(number)
+print(f"The binary representation of {number} is {binary_representation}")
