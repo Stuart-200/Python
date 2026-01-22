92a1e4c2ab5168314142b73814c7ba75d2a21199
Stuart-200 <swilson.2009@zohomail.com>
2026-01-22 20:17:17 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
-def decimal_to_binary(n):
-    if n==0:
-        return "0"
-    
-    binary = ""
-    while n > 0:
-        reminder = n%2
-        binary = str(reminder) + binary
-        n = n//2
-    return binary
+import tkinter as tk
+import random
+import string
 
-number = int(input("Enter a decimal number: "))
-binary_representation = decimal_to_binary(number)
-print(f"The binary representation of {number} is {binary_representation}")
+
+def generate_password():
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ""
+
+    for i in range(8):          # password length = 8
+        password += random.choice(characters)
+
+    result_label.config(text=password)
+
+
+
+
+
+window = tk.Tk()
+window.title("Password Generator")
+window.geometry("300x200")
+
+
+title = tk.Label(window, text="Random Password Generator")
+title.pack(pady=10)
+
+
+button = tk.Button(window, text="Generate Password", command=generate_password)
+button.pack(pady=10)
+
+
+result_label = tk.Label(window, text="", font=("Arial", 12))
+result_label.pack(pady=10)
+
+
+window.mainloop()
