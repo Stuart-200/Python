49dfb17ea53d96ea3c684b7e1fb5b44f4aa8ded3
Stuart-200 <swilson.2009@zohomail.com>
2026-01-22 20:41:31 +0600

    Add files via upload

--- hexagon.py
+++ hexagon.py
@@ -1,28 +1,25 @@
-import turtle
+import turtle 
 
 turtle.Screen().bgcolor("lightblue")
 pen = turtle.Turtle()
+pen.pensize(5) 
+pen.fillcolor("Green")
+pen.begin_fill()
 
 pen.forward(100)
-
-pen.left(120)
+pen.left(60)
 pen.forward(100)
-
-pen.left(120)
+pen.left(60)
 pen.forward(100)
-pen.penup()
-pen.right(150)
-pen.forward(50)
-
-pen.pendown()
-pen.right(90)
+pen.left(60)
 pen.forward(100)
-
-pen.right(120)
+pen.left(60)
 pen.forward(100)
-pen.right(120)
+pen.left(60)
 pen.forward(100)
 
 
+
+pen.end_fill()
 
 turtle.done()
